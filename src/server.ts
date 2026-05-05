c1831b613851091d7e1d2e337e3838c10007e6d5
mohammed-abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2026-05-05 04:13:51 +0300

    fix port problem

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ dotenv.config();
 import app from "./app";
 import connectDB from "./shared/lib/db";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 8080;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
   connectDB();
 });
